7f68d895f2632a713df352661aacb652772f4b22
i05tream <i05tream255@gmail.com>
2021-08-22 13:21:38 +0900

    19-3

---
+++ 19-3.hs
@@ -0,0 +1,9 @@
+-- 1
+concatMapF, concatMapRaw :: (a -> [b]) -> [a] -> [b]
+
+concatMapF f = foldr ((++) . f) []
+
+-- 2
+concatMapRaw _ [] = []
+concatMapRaw f (x : xs) = f x ++ concatMapRaw f xs
+
