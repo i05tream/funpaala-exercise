4da7b0391877373fd675822245aad331f29b7d10
i05tream <i05tream255@gmail.com>
2021-08-28 10:49:36 +0900

    21-5 修正

--- 21-5.hs
+++ 21-5.hs
@@ -1,8 +1,9 @@
 -- 1
-data Shape = Circle Double Double Double | Square Double Double Double | Rectangle Double Double Double Double deriving Show
+data Point = Point Double Double deriving Show
+data Shape = Circle Point Double | Square Point Double | Rectangle Point Double Double deriving Show
 
 -- 2
 square :: Shape -> Shape
-square (Rectangle x y l h)
-        | l == h = Square x y l
-        | otherwise = Rectangle x y l h
+square (Rectangle (Point x y) l h)
+        | l == h = Square (Point x y) l
+        | otherwise = Rectangle (Point x y) l h
